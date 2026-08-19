f100d90e7663888a1365245c0eeb12e7c33af720
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-08-19 18:52:16 +0900

    fix: open-localhost3000.tsにエラーハンドリングを追加
    - `child`と`open`のエラーハンドリングを追加
    - エラー出力を見やすくするためにconsolaをdevDependenciesに追加

--- dev-scripts/open-localhost.ts
+++ dev-scripts/open-localhost.ts
@@ -1,15 +1,25 @@
 import { spawn } from "node:child_process";
 import open from "open";
+import { consola } from "consola";
 
 const child = spawn("next", ["dev"], { stdio: ["inherit", "pipe", "inherit"] });
 let opened = false;
 child.stdout.on("data", (data: Buffer) => {
   const text = data.toString();
   process.stdout.write(text);
+
+  // 起動ログのReadを読んで判定しているため、バージョンアップ等で壊れる可能性あり
   if (!opened && text.includes("Ready")) {
     opened = true;
-    open("http://localhost:3000");
+    open("http://localhost:3000").catch((err) =>
+      consola.error(`localhost:3000が開けませんでした。\n${err}`),
+    );
   }
+});
+
+child.on("error", (err) => {
+  consola.error(err);
+  process.exit(1);
 });
 
 child.on("exit", (code) => process.exit(code ?? 0));
