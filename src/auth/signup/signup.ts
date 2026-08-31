20ea0b8c4b72a8d85ba7d750e9fff7f36d23e31e
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-08-31 14:27:02 +0900

    fix: メール文の相対URLを絶対URLへ修正
    Why
    - supabaseのメールアドレス認証メールの確認ページへのURLを相対URLで実装していた
    
    What
    - `toAbsoluteUrl`関数を実装
    - WebサイトURL特定用の環境変数を`.env.local`,`.env.example`へ追加
    - `emailRedirectTo`へ`toAbsoluteUrl`でラップしたUrlを渡すよう修正

--- src/auth/signup/signup.ts
+++ src/auth/signup/signup.ts
@@ -1,11 +1,12 @@
 import { createClient } from "@/supabase/server";
+import { toAbsoluteUrl } from "@/auth/handle-path";
 
 const signUpNewUser = async (email: string, password: string) => {
   const supabase = await createClient();
   const { data, error } = await supabase.auth.signUp({
     email: email,
     password: password,
-    options: { emailRedirectTo: "/signup/confirm" },
+    options: { emailRedirectTo: toAbsoluteUrl("/signup/confirm") },
   });
   return { data, error };
 };
