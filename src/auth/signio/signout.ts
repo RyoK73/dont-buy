d2b5bae3c15dba764e21408c59f9f81956cad9e9
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-08-31 12:48:15 +0900

    chore: 戻り値に関するコメントを追加

--- src/auth/signio/signout.ts
+++ src/auth/signio/signout.ts
@@ -1,6 +1,7 @@
 "use server";
 import { createClient } from "@/supabase/server";
 
+// supabase.auth.signOutのラッパー関数 errorメッセージ|undefinedを返す
 const signOut = async () => {
   const supabase = await createClient();
   const { error } = await supabase.auth.signOut();
